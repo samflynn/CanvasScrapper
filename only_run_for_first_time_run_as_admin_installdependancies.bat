00335f104358ffac1f071872b93c014389bf0092
Shrey Mudgal <shreymudgal@gmail.com>
2017-02-09 19:15:40 -0500

    Update only_run_for_first_time_run_as_admin_installdependancies.bat

--- only_run_for_first_time_run_as_admin_installdependancies.bat
+++ only_run_for_first_time_run_as_admin_installdependancies.bat
@@ -1,5 +1,8 @@
 @echo off
 echo Are you running this script as an Admin? else the script will not work.
 echo Installing beautiful soup, selenium, re and requests. Press enter to continue.
-pip install bs4 re selenium requests
+pip install bs4 
+pip install re 
+pip install selenium 
+pip install requests
 pause
